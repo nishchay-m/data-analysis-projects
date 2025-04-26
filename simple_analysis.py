505fe5ec0b0f7012effd492073cb0753bccf124f
nishchay-m <nishchayms08@gmail.com>
2025-04-26 16:03:01 +0530

    First data analysis script

---
+++ simple_analysis.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+# Sample data
+x = [1, 2, 3, 4, 5]
+y = [10, 20, 25, 30, 35]
+
+# Plotting the graph
+plt.plot(x, y)
+plt.title('Simple Line Graph')
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.show()
