48f38e8ba8d3bdb38b69af842290ce7481da68cf
nishchay-m <nishchayms08@gmail.com>
2025-04-26 16:17:10 +0530

    visualize_data.py

---
+++ visualize_data.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+# Data for plotting
+x = [1, 2, 3, 4, 5]
+y = [2, 3, 5, 7, 11]
+
+# Create a plot
+plt.plot(x, y)
+
+# Add titles and labels
+plt.title('Line Plot of Prime Numbers')
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+
+# Show the plot
+plt.show()
